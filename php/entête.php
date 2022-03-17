339cf63b1ef7ff0b21c18f2d228970b2d65f6739
ಠ_ಠ <christopher.crepin.72000@gmail.com>
2022-03-17 14:10:15 +0100

    Ajout BDD + Entête PHP

---
+++ php/entête.php
@@ -0,0 +1,44 @@
+
+<html>
+<head>
+    <meta charset="utf-8" />
+    <title>PPE04 - Red Dead Redemption II</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <link href="css/style.css" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Caveat&display=swap" rel="stylesheet">
+    <link rel="icon" href="images/navbar_icon.png" />
+</head>
+
+<body>
+    <!-- Navbar -->
+    <nav class="navbar navbar-dark bg-dark navbar-expand">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="#">
+                <img src="images/navbar_icon.png" alt="navbar_icon" height="30" class="d-inline-block align-text-top">
+            </a>
+            <ul class="navbar-nav">
+                <li class="nav-item"><a class="nav-link " href="pages/histoire.html">Histoire</a></li>
+                <li class="nav-item"><a class="nav-link " href="pages/personnages.html">Personnages</a></li>
+                <li class="nav-item"><a class="nav-link " href="pages/map.html">Map</a></li>
+                <!--<li class="nav-item"><a class="nav-link " href="pages/avis.html">Avis</a></li>-->
+            </ul>
+        </div>
+    </nav>
+    <!-- Navbar -->
+
+</body>
+
+
+</html>
+
+
+
+
+
+
+
+
+
+
+
