42329b9d1b61485655ccecd11143680cb34a4759
ಠ_ಠ <christopher.crepin.72000@gmail.com>
2022-03-30 22:46:09 +0200

    add php menu personnage

---
+++ pages/personnages - copie.php
@@ -0,0 +1,168 @@
+<html>
+
+<head>
+  <meta charset="utf-8" />
+  <title>PPE04 - Red Dead Redemption II - Personnages</title>
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet"
+    integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+  <link href="../css/style.css" rel="stylesheet">
+  <link href="https://fonts.googleapis.com/css2?family=Caveat&display=swap" rel="stylesheet">
+  <link rel="icon" href="../images/navbar_icon.png" />
+  <base href="../">
+</head>
+
+<body>
+  <!-- Navbar -->
+  <?php
+    include ('../php/entete.php');
+  ?>
+  <!-- Navbar -->
+  <!-- Content -->
+  <div class="px-5 pt-3 pb-3">
+    <div class="row" id="content">
+      <!-- Content Centre -->
+      <div class="col-2" style="border:30px solid transparent">
+        <!-- Carte 1 -->
+        <div class="card text-white bg-dark" style="width: 18rem;">
+          <a href="pages/personnages/arthur_morgan.php">
+            <img src="images/rdr2_arthur_morgan_256x256.png" class="card-img-top" alt="Arthur Morgan">
+          </a>
+          <div class="card-body">
+            <h5 class="card-title">Arthur Morgan</h5>
+            <p class="card-text">Homme de main le plus expérimenté et le plus fiable de la bande. Froid et impitoyable,
+              il possède son propre sens de l'honneur. C'est le personnage principal du jeu.</p>
+          </div>
+        </div>
+        <!-- Carte 1 -->
+        </br>
+        <!-- Carte 2 -->
+        <div class="card text-white bg-dark" style="width: 18rem;">
+          <a href="pages/personnages/hosea_mathews.php">
+            <img src="images/rdr2_hosea_matthews_256x256.png" class="card-img-top" alt="Hosea Matthews">
+          </a>
+          <div class="card-body">
+            <h5 class="card-title">Hosea Matthews</h5>
+            <p class="card-text">Roi de l'escroquerie, oscillant entre gentleman et brigand, Hosea est le meilleur ami
+              de Dutch, ainsi que son bras droit depuis plus de vingt ans.</p>
+          </div>
+        </div>
+        <!-- Carte 2 -->
+      </div>
+      <div class="col-2" style="border:30px solid transparent">
+        <!-- Carte 3 -->
+        <div class="card text-white bg-dark" style="width: 18rem;">
+          <a href="pages/personnages/dutch_van_der_linde.php">
+            <img src="images/rdr2_dutch_van_der_linde_256x256.png" class="card-img-top" alt="Dutch van der Linde">
+          </a>
+          <div class="card-body">
+            <h5 class="card-title">Dutch van der Linde</h5>
+            <p class="card-text" id="text_card">Chef d'une grande bande de hors-la-loi et de renégats. Il semble
+              néanmoins commencer à montrer des faiblesse face à l'avènement du monde moderne.</p>
+          </div>
+        </div>
+        <!-- Carte 3 -->
+        </br>
+        <!-- Carte 4 -->
+        <div class="card text-white bg-dark" style="width: 18rem;">
+          <a href="pages/personnages/sean_macguire.php">
+            <img src="images/rdr2_sean_macguire_256x256.png" class="card-img-top" alt="Sean Macguire">
+          </a>
+          <div class="card-body">
+            <h5 class="card-title">Sean Macguire</h5>
+            <p class="card-text">Insolent, ce jeune braqueur irlandais descend d'une longue lignée de criminels et
+              d'opposants politiques. Il cherche toujours à prendre part à l'action et se montre très sûr de lui...</p>
+          </div>
+        </div>
+        <!-- Carte 4 -->
+      </div>
+      <div class="col-2" style="border:30px solid transparent">
+        <!-- Carte 5 -->
+        <div class="card text-white bg-dark" style="width: 18rem;">
+          <a href="pages/personnages/sadie_adler.php">
+            <img src="images/rdr2_sadie_adler_256x256.png" class="card-img-top" alt="Sadie Adler">
+          </a>
+          <div class="card-body">
+            <h5 class="card-title">Sadie Adler</h5>
+            <p class="card-text">Une veuve qui cherche à tout prix à venger la mort de son mari. Implacable, elle n'a
+              peur de rien ni de personne. De mauvais augure pour ceux qui croisent sa route.</p>
+          </div>
+        </div>
+        <!-- Carte 5 -->
+        </br>
+        <!-- Carte 6 -->
+        <div class="card text-white bg-dark" style="width: 18rem;">
+          <a href="pages/personnages/john_marston.php">
+            <img src="images/rdr2_john_marston_256x256.png" class="card-img-top" alt="John Marston">
+          </a>
+          <div class="card-body">
+            <h5 class="card-title">John Marston</h5>
+            <p class="card-text">Avant d'être recueilli par Dutch à l'âge de 12 ans, John survivait par ses propres
+              moyens dans la rue. Lui et Arthur sont les deux protégés favoris de Dutch.</p>
+          </div>
+        </div>
+        <!-- Carte 6 -->
+      </div>
+      <div class="col-2" style="border:30px solid transparent">
+        <!-- Carte 7 -->
+        <div class="card text-white bg-dark" style="width: 18rem;">
+          <a href="pages/personnages/charles_smith.php">
+            <img src="images/rdr2_charles_smith_256x256.png" class="card-img-top" alt="Charles Smith">
+          </a>
+          <div class="card-body">
+            <h5 class="card-title">Charles Smith</h5>
+            <p class="card-text">Charles est un homme discret et réservé qui se montre extrêmement compétent dans tout
+              ce qu'il entreprend et qui s'avère quasiment imbattable au combat.</p>
+          </div>
+        </div>
+        <!-- Carte 7 -->
+        </br>
+        <!-- Carte 8 -->
+        <div class="card text-white bg-dark" style="width: 18rem;">
+          <a href="pages/personnages/micah_bell.php">
+            <img src="images/rdr2_micah_bell_256x256.png" class="card-img-top" alt="Micah Bell">
+          </a>
+          <div class="card-body">
+            <h5 class="card-title">Micah Bell</h5>
+            <p class="card-text">Criminel et tueur à gages professionnel. Indomptable et imprévisible, il ne vit que
+              pour l'action. Tête influente au sein du gang, il sera le principal antagoniste du jeu.</p>
+          </div>
+        </div>
+        <!-- Carte 8 -->
+      </div>
+      <!-- Content Centre -->
+      <!-- Content Droit -->
+      <div class="col-4" style="border:30px solid transparent">
+        <!-- top -->
+        <p id="index_content_droite_paragraphe"><img src="images/bande-de-van-der-linde-m.jpg"
+            style="border:30px solid transparent;  max-width: 90%; max-height: 90%;" /></p>
+        <!-- top -->
+        <hr />
+        <!-- middle -->
+        <p class="text-center" id="text" style="border:15px solid transparent;">
+          L'histoire de Red Dead Redemption 2 est centrée autour de la bande de Van der Linde. C'est un groupe de
+          hors-la-loi, de renégats et de marginaux avec à leur tête le charismatique Dutch van der Linde. Après avoir
+          longtemps vécu au-dessus des lois, ils craignent désormais d'avoir à le payer.
+        </p>
+        <!-- middle -->
+        <hr />
+        <!-- bottom -->
+        <p class="text-center" id="text" style="border:15px solid transparent;">
+          « Si nous devons nous battre, nous nous battons. Si nous devons courir, nous courrons. Si nous devons mourir,
+          nous mourrons. Mais … nous resterons libres. » – Javier Escuella
+        </p>
+        <!-- bottom -->
+      </div>
+      <!-- Content Droit -->
+    </div>
+  </div>
+  <!-- Content -->
+  <!-- Footer -->
+  <footer class="bg-dark">
+    <div class="text-center p-2 footer-text">
+      © PPE04 - Dorian Martin et Christopher Crépin - SIO Slam 2022
+    </div>
+  </footer>
+  <!-- Footer -->
+</body>
+
+</html>
--- pages/personnages.php
+++ pages/personnages.php
@@ -22,112 +22,44 @@
     <div class="row" id="content">
       <!-- Content Centre -->
       <div class="col-2" style="border:30px solid transparent">
-        <!-- Carte 1 -->
-        <div class="card text-white bg-dark" style="width: 18rem;">
-          <a href="pages/personnages/arthur_morgan.php">
-            <img src="images/rdr2_arthur_morgan_256x256.png" class="card-img-top" alt="Arthur Morgan">
+        <?php
+        
+        include ("../php/connexion.php");
+
+        $req = $bdd->prepare("SELECT * FROM personnages");
+        $req->execute();
+        $personnages = $req->fetchAll();
+        
+        $n=0;
+
+        foreach ($personnages as $personnage){
+          $nom_perso = $personnage["nom_perso"];
+          $resume_perso = $personnage["resume_perso"];
+          $image_perso = $personnage["image_perso"];
+          $lien_perso = $personnage["lien_perso"];
+          
+          if ($n==2) {
+            echo
+            "</div>
+            <div class=\"col-2\" style=\"border:30px solid transparent\">";
+            $n=0;
+          }
+
+          echo
+          "<div class=\"card text-white bg-dark\" style=\"width: 18rem;\">
+          <a href=\"$lien_perso\">
+            <img src=\"$image_perso\" class=\"card-img-top\" alt=\"Arthur Morgan\">
           </a>
-          <div class="card-body">
-            <h5 class="card-title">Arthur Morgan</h5>
-            <p class="card-text">Homme de main le plus expérimenté et le plus fiable de la bande. Froid et impitoyable,
-              il possède son propre sens de l'honneur. C'est le personnage principal du jeu.</p>
+          <div class=\"card-body\">
+            <h5 class=\"card-title\">$nom_perso</h5>
+            <p class=\"card-text\">$resume_perso</p>
+            </div>
           </div>
-        </div>
-        <!-- Carte 1 -->
-        </br>
-        <!-- Carte 2 -->
-        <div class="card text-white bg-dark" style="width: 18rem;">
-          <a href="pages/personnages/hosea_mathews.php">
-            <img src="images/rdr2_hosea_matthews_256x256.png" class="card-img-top" alt="Hosea Matthews">
-          </a>
-          <div class="card-body">
-            <h5 class="card-title">Hosea Matthews</h5>
-            <p class="card-text">Roi de l'escroquerie, oscillant entre gentleman et brigand, Hosea est le meilleur ami
-              de Dutch, ainsi que son bras droit depuis plus de vingt ans.</p>
-          </div>
-        </div>
-        <!-- Carte 2 -->
-      </div>
-      <div class="col-2" style="border:30px solid transparent">
-        <!-- Carte 3 -->
-        <div class="card text-white bg-dark" style="width: 18rem;">
-          <a href="pages/personnages/dutch_van_der_linde.php">
-            <img src="images/rdr2_dutch_van_der_linde_256x256.png" class="card-img-top" alt="Dutch van der Linde">
-          </a>
-          <div class="card-body">
-            <h5 class="card-title">Dutch van der Linde</h5>
-            <p class="card-text" id="text_card">Chef d'une grande bande de hors-la-loi et de renégats. Il semble
-              néanmoins commencer à montrer des faiblesse face à l'avènement du monde moderne.</p>
-          </div>
-        </div>
-        <!-- Carte 3 -->
-        </br>
-        <!-- Carte 4 -->
-        <div class="card text-white bg-dark" style="width: 18rem;">
-          <a href="pages/personnages/sean_macguire.php">
-            <img src="images/rdr2_sean_macguire_256x256.png" class="card-img-top" alt="Sean Macguire">
-          </a>
-          <div class="card-body">
-            <h5 class="card-title">Sean Macguire</h5>
-            <p class="card-text">Insolent, ce jeune braqueur irlandais descend d'une longue lignée de criminels et
-              d'opposants politiques. Il cherche toujours à prendre part à l'action et se montre très sûr de lui...</p>
-          </div>
-        </div>
-        <!-- Carte 4 -->
-      </div>
-      <div class="col-2" style="border:30px solid transparent">
-        <!-- Carte 5 -->
-        <div class="card text-white bg-dark" style="width: 18rem;">
-          <a href="pages/personnages/sadie_adler.php">
-            <img src="images/rdr2_sadie_adler_256x256.png" class="card-img-top" alt="Sadie Adler">
-          </a>
-          <div class="card-body">
-            <h5 class="card-title">Sadie Adler</h5>
-            <p class="card-text">Une veuve qui cherche à tout prix à venger la mort de son mari. Implacable, elle n'a
-              peur de rien ni de personne. De mauvais augure pour ceux qui croisent sa route.</p>
-          </div>
-        </div>
-        <!-- Carte 5 -->
-        </br>
-        <!-- Carte 6 -->
-        <div class="card text-white bg-dark" style="width: 18rem;">
-          <a href="pages/personnages/john_marston.php">
-            <img src="images/rdr2_john_marston_256x256.png" class="card-img-top" alt="John Marston">
-          </a>
-          <div class="card-body">
-            <h5 class="card-title">John Marston</h5>
-            <p class="card-text">Avant d'être recueilli par Dutch à l'âge de 12 ans, John survivait par ses propres
-              moyens dans la rue. Lui et Arthur sont les deux protégés favoris de Dutch.</p>
-          </div>
-        </div>
-        <!-- Carte 6 -->
-      </div>
-      <div class="col-2" style="border:30px solid transparent">
-        <!-- Carte 7 -->
-        <div class="card text-white bg-dark" style="width: 18rem;">
-          <a href="pages/personnages/charles_smith.php">
-            <img src="images/rdr2_charles_smith_256x256.png" class="card-img-top" alt="Charles Smith">
-          </a>
-          <div class="card-body">
-            <h5 class="card-title">Charles Smith</h5>
-            <p class="card-text">Charles est un homme discret et réservé qui se montre extrêmement compétent dans tout
-              ce qu'il entreprend et qui s'avère quasiment imbattable au combat.</p>
-          </div>
-        </div>
-        <!-- Carte 7 -->
-        </br>
-        <!-- Carte 8 -->
-        <div class="card text-white bg-dark" style="width: 18rem;">
-          <a href="pages/personnages/micah_bell.php">
-            <img src="images/rdr2_micah_bell_256x256.png" class="card-img-top" alt="Micah Bell">
-          </a>
-          <div class="card-body">
-            <h5 class="card-title">Micah Bell</h5>
-            <p class="card-text">Criminel et tueur à gages professionnel. Indomptable et imprévisible, il ne vit que
-              pour l'action. Tête influente au sein du gang, il sera le principal antagoniste du jeu.</p>
-          </div>
-        </div>
-        <!-- Carte 8 -->
+          <br>";
+
+          $n++;
+        }
+        ?>
       </div>
       <!-- Content Centre -->
       <!-- Content Droit -->
